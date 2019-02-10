1e5fe3c854ed0069c84b71a5c1b9230f217d0c4e
skzmc <s.kzmchv@gmail.com>
2019-02-10 19:58:21 +0300

    up

---
+++ main.py
@@ -0,0 +1,291 @@
+from myParser import parse
+from bisect import bisect_left
+from itertools import chain, combinations, product
+
+
+
+def takeClosest(myList, myNumber):
+    """
+    Assumes myList is sorted. Returns closest value to myNumber.
+
+    If two numbers are equally close, return the smallest number.
+    """
+    pos = bisect_left(myList, myNumber)
+    if pos == 0:
+        return myList[0]
+    if pos == len(myList):
+        return myList[-1]
+    before = myList[pos - 1]
+    after = myList[pos]
+    if after - myNumber < myNumber - before:
+       return after
+    else:
+       return before
+
+if __name__ == "__main__":
+
+    devicesTimestampsSuccess = parse('syslog_dif_dBm_SF7','out')
+    # print(devicesTimestampsSuccess)
+    begin = 539266819
+    end = 1430876615
+    step = 7000000
+    e = 100000
+
+    timeOnAir = 119000
+    i = 3
+    devicesTimestampsFail = {}
+    for key in devicesTimestampsSuccess:
+        # print(key)
+        devicesTimestampsSuccess[key] = [int(entry) for entry in devicesTimestampsSuccess[key]]
+        # print(devicesTimestampsSuccess[key])
+
+        devicesTimestampsFail[key] = []
+        nextTimestamp = devicesTimestampsSuccess[key][0] - step
+
+        while nextTimestamp > begin:
+            devicesTimestampsFail[key].append(nextTimestamp)
+            nextTimestamp = nextTimestamp - step
+        devicesTimestampsFail[key].reverse()
+        nextTimestamp = devicesTimestampsSuccess[key][0] + step
+        while nextTimestamp < end:
+            if nextTimestamp - takeClosest(devicesTimestampsSuccess[key],nextTimestamp) > e:
+                devicesTimestampsFail[key].append(nextTimestamp)
+            nextTimestamp = nextTimestamp + step
+
+        # print(devicesTimestampsFail[key])
+    devicesTimestampsFail.pop('-1 SF7BW125')
+    # for entry in devicesTimestampsSuccess['-1 SF7BW125']:
+    #     flag = True
+    #     for key in devicesTimestampsFalse:
+    #         if key == '-1 SF7BW125':
+    #             continue
+    #         if len(devicesTimestampsFalse[key]) != 0 and abs(entry - takeClosest(devicesTimestampsFalse[key],entry)) < e:
+    #             flag = False
+    #     if flag:
+    #         print(str(entry) + ' not find')
+
+    f = open('outFailText.txt', 'w')
+    for entry in devicesTimestampsFail:
+        f.write(entry + ' ' + str(len(devicesTimestampsFail[entry])) + ' ' + str(devicesTimestampsFail[entry]) + '\n')
+    f.close()
+
+
+    m = sum(len(entry) for entry in devicesTimestampsSuccess)
+    print(m)
+
+    part = timeOnAir / i
+
+    # test
+    # timeOnAir = 10
+    # devicesTimestampsSuccess ={'1': [100,200,300], '2': [400,500,600], '3': [399,695,797], '4':[300]}
+    # devicesTimestampsFail ={'1': [430,540,640], '2': [700,800,900], '3':[], '4':[790]}
+    # test
+
+
+
+
+    for n in range(3):
+        if n == 0:
+            continue
+
+        dict = {}
+        listForEntry = [0]*n
+        successX = [0] * i
+        failX = [0] * i
+        crossTimestapsSuccess = {}
+        crossTimestapsFail = {}
+        for key in devicesTimestampsSuccess:
+            for entry in devicesTimestampsSuccess[key]:
+                crossTimestapsSuccess[entry] = []
+                numberOfParticipant = 0
+                tmpSuccessX = [0]*i
+                listForEntry = [''] * n
+
+                flag = False
+
+                for currDevice in devicesTimestampsSuccess:
+
+                    if key == currDevice:
+                        continue
+
+                    nearestTimestamps = takeClosest(devicesTimestampsSuccess[currDevice],entry)
+                    x = nearestTimestamps - entry
+
+                    if x < 0 and abs(x) < timeOnAir:
+                        # print("Attention " + str(entry) + " " + str(nearestTimestamps))
+                        flag = True
+                        break
+
+                    if x < timeOnAir and x > 0:
+                        numberOfParticipant = numberOfParticipant + 1
+
+                        if numberOfParticipant > n:
+                            # crossTimestaps.pop(entry)
+                            break
+
+                        j = 0
+                        while x > part*(j+1):
+                            j = j + 1
+                        tmpSuccessX[j] = tmpSuccessX[j] + 1
+                        listForEntry[numberOfParticipant - 1] = str((j+1)*part)
+                        crossTimestapsSuccess[entry].append(nearestTimestamps)
+
+                if numberOfParticipant > n or flag:
+                    crossTimestapsSuccess.pop(entry)
+                    continue
+
+                for currDevice in devicesTimestampsFail:
+
+                    if key == currDevice:
+                        continue
+
+                    if len(devicesTimestampsFail[currDevice]) == 0:
+                        continue
+
+                    nearestTimestamps = takeClosest(devicesTimestampsFail[currDevice],entry)
+                    x = nearestTimestamps - entry
+
+                    if x < 0 and abs(x) < timeOnAir:
+                        flag = True
+                        break
+
+                    if x < timeOnAir and x > 0:
+                        numberOfParticipant = numberOfParticipant + 1
+
+                        if numberOfParticipant > n:
+                            crossTimestapsSuccess.pop(entry)
+                            break
+
+                        j = 0
+                        while x > part*(j+1):
+                            j = j + 1
+                        tmpSuccessX[j] = tmpSuccessX[j] + 1
+                        listForEntry[numberOfParticipant - 1] = str((j+1)*part)
+                        crossTimestapsSuccess[entry].append(nearestTimestamps)
+
+                if numberOfParticipant < n or flag:
+                    crossTimestapsSuccess.pop(entry)
+                    continue
+                if numberOfParticipant == n:
+                    for j in range(i):
+                        successX[j] = successX[j] + tmpSuccessX[j]
+
+                    res = ' '.join(listForEntry)
+                    if dict.get(res) == None:
+                        dict[res] = [1, 0]
+                    else:
+                        dict[res][0] += 1
+
+        for key in devicesTimestampsFail:
+            for entry in devicesTimestampsFail[key]:
+                crossTimestapsFail[entry] = []
+                numberOfParticipant = 0
+                tmpFailX = [0]*i
+                listForEntry = [''] * n
+                flag = False
+                for currDevice in devicesTimestampsSuccess:
+
+                    if key == currDevice:
+                        continue
+
+                    nearestTimestamps = takeClosest(devicesTimestampsSuccess[currDevice],entry)
+                    x = nearestTimestamps - entry
+
+                    if x < 0 and abs(x) < timeOnAir:
+                        flag = True
+                        break
+
+                    if x < timeOnAir and x > 0:
+                        numberOfParticipant = numberOfParticipant + 1
+
+                        if numberOfParticipant > n:
+                            break
+
+                        j = 0
+                        while x > part*(j+1):
+                            j = j + 1
+                        tmpFailX[j] = tmpFailX[j] + 1
+                        listForEntry[numberOfParticipant - 1] = str((j+1)*part)
+                        crossTimestapsFail[entry].append(nearestTimestamps)
+
+                if numberOfParticipant > n or flag:
+                    crossTimestapsFail.pop(entry)
+                    continue
+                for currDevice in devicesTimestampsFail:
+
+                    if key == currDevice:
+                        continue
+
+                    if len(devicesTimestampsFail[currDevice]) == 0:
+                        continue
+
+                    nearestTimestamps = takeClosest(devicesTimestampsFail[currDevice],entry)
+                    x = nearestTimestamps - entry
+
+                    if x < 0 and abs(x) < timeOnAir:
+                        flag = True
+                        break
+
+                    if x < timeOnAir and x > 0:
+                        numberOfParticipant = numberOfParticipant + 1
+
+                        if numberOfParticipant > n:
+                            crossTimestapsFail.pop(entry)
+                            break
+
+                        j = 0
+                        while x > part*(j+1):
+                            j = j + 1
+                        tmpFailX[j] = tmpFailX[j] + 1
+                        listForEntry[numberOfParticipant - 1] = str((j+1)*part)
+                        crossTimestapsFail[entry].append(nearestTimestamps)
+
+                if numberOfParticipant < n or flag:
+                    crossTimestapsFail.pop(entry)
+                    continue
+                if numberOfParticipant == n:
+                    for j in range(i):
+                        failX[j] = failX[j] + tmpFailX[j]
+
+                    res = ' '.join(listForEntry)
+                    if dict.get(res) == None:
+                        dict[res] = [0, 1]
+                    else:
+                        dict[res][1] += 1
+
+        print('n = ' + str(n))
+        if n == 1:
+            print('Success  = ' + str(successX))
+            print('Fail = ' + str(failX))
+        # else:
+        #     for j in dict:
+        #         print(j + ' Success: ' + str(dict[j][0]) + ' Fail: ' + str(dict[j][1]))
+        #
+        # Pr = [0.0] * i
+        # for j in range(i):
+        #     if successX[j] + failX[j] != 0:
+        #         Pr[j] = successX[j] / (successX[j] + failX[j])
+        #         print('Pr[success | x = {}] = {}'.format((j+1)*part,float(Pr[j])))
+
+        # print('success transfer')
+        #for crossTimestap in crossTimestapsSuccess:
+         #   if len(crossTimestapsSuccess[crossTimestap]) != 0:
+          #      print(str(crossTimestap) + ': ' + str(crossTimestapsSuccess[crossTimestap]))
+
+        # print('fail transfer')
+        # for crossTimestap in crossTimestapsFail:
+        #   if len(crossTimestapsFail[crossTimestap]) != 0:
+        #       print(str(crossTimestap) + ': ' + str(crossTimestapsFail[crossTimestap]))
+
+        for val in product(range(i), repeat=n):
+            strVal = ''
+            for j in range(len(val)-1):
+                strVal = strVal + str((val[j]+1)*part) + ' '
+            strVal = strVal +  str((val[-1]+1)*part)
+            # print(strVal)
+            # print(val + ' Success: ' + str(dict[j][0]) + ' Fail: ' + str(dict[j][1]))
+            if dict.get(strVal) != None:
+                print('Pr[success | ' + strVal + '] = ' + str(dict[strVal][0] / (dict[strVal][0] + dict[strVal][1])))
+        # for entry in dict:
+
+            # print('Pr[success | ' + entry + '] = ' + str(dict[entry][0]/(dict[entry][0] + dict[entry][1])))
---
+++ myParser.py
@@ -0,0 +1,62 @@
+import csv
+import json
+
+
+def csv_dict_writer(path, fieldnames, data):
+    """
+    Writes a CSV file using DictWriter
+    """
+    with open(path, "w", newline='') as out_file:
+        writer = csv.DictWriter(out_file, delimiter=',', fieldnames=fieldnames)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
+
+
+
+def parse(inputFile, outputFile):
+    fieldnames = ['id device',   'num of message', 'timestamps']
+    data = [' '] * 3
+    res_list = []
+    devicesTimestamps = {}
+    devicesMessage = {}
+    messageCounter = 0
+    currDevice = '-1'
+
+    f = open(inputFile)
+    for line in f:
+        if line.find("fcnt") != -1:
+            currDevice = line.split(' ')[-2]
+            continue
+
+        if line.find("JSON up: {\"rxpk\"") != -1:
+            messageCounter += 1
+            tmp = line.split('JSON up:')
+            d = json.loads(tmp[1])
+
+            currDevice = currDevice + ' ' + d['rxpk'][0]['datr']
+
+            if devicesTimestamps.get(currDevice) == None:
+                devicesTimestamps[currDevice] = [str(d['rxpk'][0]['tmst'])]
+                devicesMessage[currDevice] = 1
+            else:
+                devicesTimestamps[currDevice].append(str(d['rxpk'][0]['tmst']))
+                devicesMessage[currDevice] += 1
+
+            currDevice = '-1'
+            continue
+    f.close()
+
+    for key in devicesMessage.keys():
+        data[0] = key
+        data[1] = str(devicesMessage[key])
+        data[2] = ' '.join(devicesTimestamps[key])
+        inner_dict = dict(zip(fieldnames, data))
+        res_list.append(inner_dict)
+    #csv_dict_writer(outputFile + 'Table.csv', fieldnames, res_list)
+
+    f = open(outputFile + 'Text.txt', 'w')
+    for entry in res_list:
+        f.write(entry['id device'] + ' ' + entry['num of message'] + ' ' + entry['timestamps'] + '\n')
+    f.close()
+    return devicesTimestamps
